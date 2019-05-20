6a58224128f271d59081e5d09fb8b618d89f1593
oleksii.khorunzhak <oleksii.khorunzhak@fortegrp.net>
2019-05-20 14:00:52 +0300

    img

--- alex.php
+++ alex.php
@@ -29,13 +29,19 @@
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
 <script src="js/plugins.js"></script>
 <script src="js/main.js"></script>
-
 <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 <script>
     window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
     ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
 </script>
 <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+
+<img src="https://www.iconfinder.com/icons/4633881/advertising_analysis_business_chart_marketing_promotion_research_icon" alt ="">
+
+
 </body>
+
+
+
 
 </html>
