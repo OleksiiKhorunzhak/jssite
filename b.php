b879d10229e18f5810cf691c2264decd12092f5c
borys.pidperygora <borys.pidperygora@forte.net>
2019-07-08 19:32:01 +0300

    add first JS and add bootstrap

--- b.php
+++ b.php
@@ -25,17 +25,41 @@
 
 <div class="container ">
     <div class="row height align-content-center justify-content-center my-work-row">
-        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded"  src="https://picsum.photos/200?random1" alt=""></p>
-        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded"  src="https://picsum.photos/200?random2" alt=""></p>
-        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded"  src="https://picsum.photos/200?random3" alt=""></p>
-        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded"  src="https://picsum.photos/200?random4" alt=""></p>
-        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded"  src="https://picsum.photos/200?random5" alt=""></p>
-        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded"  src="https://picsum.photos/200?random6" alt=""></p>
+        <div class="col-12">
+            <p>When you click on the picture, the boarder should be added around</p>
+        </div>
+        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded my_click"  src="https://picsum.photos/200?random1" alt=""></p>
+        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded my_click"  src="https://picsum.photos/200?random2" alt=""></p>
+        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded my_click"  src="https://picsum.photos/200?random3" alt=""></p>
+        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded my_click"  src="https://picsum.photos/200?random4" alt=""></p>
+        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded my_click"  src="https://picsum.photos/200?random5" alt=""></p>
+        <p class="co-6 col-sm-3 col-md-4 mt-2"><img class="rounded my_click"  src="https://picsum.photos/200?random6" alt=""></p>
     </div>
+
 </div>
 
+<div class="container">
+    <div class="row height align-content-center justify-content-center">
+        <div class="col-8">
+            <p>Show mouse coordinates in the block</p>
+            <p>
+                <span>X: <span id="offx">0</span></span>
+                <span>Y: <span id="offy">0</span></span>
+            </p>
+        </div>
 
+        <div class="col-8 h-50 border border-dark" id="Work_1">
+            <div id="mousediv"></div>
+        </div>
+    </div>
 
+</div>
+
+<div class="container">
+    <div class="row height align-content-center justify-content-center">
+        div.
+    </div>
+</div>
 
 
 
--- js/bjs/1.js
+++ js/bjs/1.js
@@ -1,12 +1,2 @@
-document.onclick = function (event) {
-    console.dir(event.target);
-    if (event.target.tagName == "IMG" && event.target.classList.contains('border', 'border-warning')){
-        event.target.classList.remove('border', 'border-warning')
-    }else{
-        if (event.target.tagName == "IMG"){
-            event.target.classList.add('border', 'border-warning');
-        }
-    }
 
 
-};
